3552973e89b7dc2bb676d2f979b4c7743c7f633f
unknown <iamayushsingh03@gmail.com>
2023-03-28 08:12:32 +0530

    LL 1

--- 25.LinkList 1/LinkedList.java
+++ 25.LinkList 1/LinkedList.java
@@ -120,6 +120,74 @@ public class LinkedList{
         }
         System.out.println("null");
     }
+
+    public int itrSearch(int key){
+        Node temp = head;
+        int i=0;
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next; // age ki ref ki taraf point karta hai
+            i++;
+        }
+        return -1;
+    }
+    public int helper(Node head , int key){ 
+        //it is doing the main work of the recursive func
+        //base case
+        if(head==null){ //key not exist in ll
+            return -1;
+        }
+        //kaam
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx +1;
+    }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
+    public void reverse(){
+        Node prev = null;
+        Node curr =tail=head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev =curr;
+            curr =next;
+        }
+        head = prev;
+    }
+
+    public void deleteNthFromEnd(int N){
+        //calculate size
+        int size = 0;
+        Node temp = head ; 
+        while(temp != null){
+            temp = temp.next;
+            size++;
+        }
+
+        //size- n
+        int i = 1;
+        int idxToFind = size - N;
+        Node prev = head;
+        while(i < idxToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.head = new Node(1);
@@ -135,11 +203,16 @@ public class LinkedList{
         ll.addLast(4);
         ll.add(2, 9);   //call this methods at last for better results
         ll.print();
-        ll.removeFirst();
+        // ll.removeFirst();
+        // ll.print();
+        // ll.removeLast();
+        ll.reverse();
         ll.print();
-        ll.removeLast();
+        ll.deleteNthFromEnd(4);
         ll.print();
-        System.out.println(ll.size);
+        // System.out.println(ll.itrSearch(3));
+        // System.out.println(ll.itrSearch(9));
+        // System.out.println(ll.size);
     
     }
 }
