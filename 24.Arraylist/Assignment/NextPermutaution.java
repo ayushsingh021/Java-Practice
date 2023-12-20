fa50271ca37598cb54a21df93437f90913a1d2d7
Ayush021 <12114021@nitkkr.ac.in>
2023-12-20 11:09:15 +0530

    nextPermutaion updated sol

--- 24.Arraylist/Assignment/NextPermutaution.java
+++ 24.Arraylist/Assignment/NextPermutaution.java
@@ -5,45 +5,38 @@
 // Output: [1,3,2]
 
 class Solution {
-    public static void reverseArr(int [] nums , int si ,int ei ){
-        while(si < ei){
-            //swap
-             int temp = nums[si];
-                nums[si] = nums[ei];
-                nums[ei] = temp;
-                si++;
-                ei--;
+    public static void swap(int nums[] , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void reverse(int nums[] , int start , int end){
+        int i = start;
+        int j = end ; 
+        while( i<= j ){
+            swap(nums , i , j );
+            i++;
+            j--;
         }
     }
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int idx = -1;
-        //1. Finding the break point
-        for(int i  = n-2; i>= 0 ; i--){
-            if(nums[i] < nums[i+1]){
-                idx = i;
-                break;
-            }
-        }
-        System.out.println(idx);
+        if(nums.length <= 1) return;
 
-        if(idx == -1){
-            reverseArr(nums, 0 , n-1);
-        }
-        else{  
-            //2. Swaping with min from back
-             for(int j = n-1 ; j > idx; j--){
-             if(nums[j] > nums[idx]){     
-                // swap
-                int temp = nums[j];
-                nums[j] = nums[idx];
-                nums[idx] = temp;
-                break;
-                }
-            }
-            //3.reverse the remaining array
-            reverseArr(nums , idx + 1 , n-1);
-        }  
-      
+        int i=nums.length-2;
+        int j = nums.length -1;
+    while(i >= 0 && nums[i]>= nums[j]){
+        i--;
+        j--;
+    }
+   
+
+    if( i>= 0 ){
+        j = nums.length-1;
+        while(j>= 0 && nums[i] >= nums[j]) j--;
+        swap(nums, i , j);
+    }
+
+    reverse( nums , i+1 , nums.length-1);
+
     }
 }
